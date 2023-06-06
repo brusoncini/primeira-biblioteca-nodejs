34b01fd1e11a36c670f735be6a2e4eb2ae6c3e72
Bruna Soncini <brusoncini@gmail.com>
2023-06-06 11:21:49 -0300

    first commit

---
+++ src/http-validacao.js
@@ -0,0 +1,21 @@
+function extraiLinks(arrLinks) {
+  return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
+}
+
+async function checaStatus(listaUrls) {
+  const arrStatus = await Promise.all (
+    listaUrls.map(async (url) => {
+      const response = await fetch(url);
+      return response.status;
+    })
+  );
+  return arrStatus;
+}
+
+export default async function listaValidada(listaDeLinks) {
+  const links = extraiLinks(listaDeLinks);
+  const status = await checaStatus(links);
+  return status;
+}
+
+//[gatinho salsicha](http://gatinhosalsicha.com.br/);
